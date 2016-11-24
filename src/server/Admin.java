59b021b6b0817cca875512344a7ae24c4b5454ac
Angelo S. <boyangel90@hotmail.it>
2016-11-24 21:56:15 +0100

    User class

---
+++ src/server/Admin.java
@@ -0,0 +1,12 @@
+package server;
+
+public class Admin extends User {
+
+	public Admin(String username, String password, String name, String surname) {
+		super(username, password, name, surname);
+	}
+
+	public void doSomething() {
+	}
+	
+}
--- src/server/User.java
+++ src/server/User.java
@@ -1,7 +1,47 @@
 package server;
 
+import javax.jws.WebService;
+import javax.xml.ws.soap.Addressing;
+
+import com.sun.xml.ws.developer.Stateful;
+import com.sun.xml.ws.developer.StatefulWebServiceManager;
+
+@WebService
+@Stateful
+@Addressing
 public abstract class User {
 
-	
-	
+	public static StatefulWebServiceManager<User> manager;
+	private String username;
+	private String password;
+	private String name;
+	private String surname;
+
+	public User(String username, String password, String name, String surname) {
+		this.username = username;
+		this.password = password;
+		this.name = name;
+		this.surname = surname;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
 }
